a50600228208fd2e10712864c81ee709fff79b84
krishsharma955 <krishsharma9005@gmail.com>
2026-02-27 19:13:04 +0530

    Project Setup

---
+++ src/main/java/com/K955/Placement_Portal/PlacementPortalApplication.java
@@ -0,0 +1,13 @@
+package com.K955.Placement_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/K955/Placement_Portal/PlacementPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K955.Placement_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
